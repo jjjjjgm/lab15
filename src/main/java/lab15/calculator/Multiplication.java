479280ab6ada717dd7f4cfb9ff8ccbec04339aaa
jargalmaa <jagamaasaikhan@gmail.com>
2025-05-09 12:03:03 +0800

    division daraagiin commit

--- src/main/java/lab15/calculator/Multiplication.java
+++ src/main/java/lab15/calculator/Multiplication.java
@@ -4,10 +4,4 @@ public class Multiplication {
     public double multiply(double a, double b) {
         return a * b;
     }
-    public double add(double a, double b) {
-        return a + b;
-    }
-    public double sub(double a, double b) {
-        return a - b;
-    }
 }
